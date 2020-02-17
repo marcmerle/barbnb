0602aa247069c5ed8ac70761d39462270c5f6e87
Marc Merle <marcmerle@hotmail.com>
2020-02-17 17:33:19 +0100

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'dashboards/:id', to: 'dashboards#show'
-  get 'dashboards/:id/edit', to: 'dashboards#edit'
-  patch 'dashboards/:id', to: 'dashboards#update'
-  delete '/', to: 'dashbboards#delete'
-  devise_for :users
   root to: 'pages#home'
+  devise_for :users
+
+  get '/dashboard', to: 'bookings#index'
+
   resources :bars, except: :destroy
-  get 'bookings/show'
-  get 'bookings/edit'
+  resources :bookings, except: :index
 end
