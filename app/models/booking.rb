9a7d00053660543a057ab5040188b407625ddd64
Lomig <dunwael@gmail.com>
2020-02-17 16:43:09 +0100

    Fixing typo (:amout -> :amount in validation)

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :bar
   belongs_to :user
 
-  validates: :amout, :starts_at, :ends_at, :state, :guest_number, presence: true
+  validates: :amount, :starts_at, :ends_at, :state, :guest_number, presence: true
 end
