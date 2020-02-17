f7c43639bdf8dfb51aa90fd044484b1a68bc3b4f
Marc Merle <marcmerle@hotmail.com>
2020-02-17 17:08:55 +0100

    added dashboards controller, dashboard routes and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'dashboards/:id', to: 'dashboards#show'
+  get 'dashboards/:id/edit', to: 'dashboards#edit'
+  patch 'dashboards/:id', to: 'dashboards#update'
+  delete '/', to: 'dashbboards#delete'
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
