e8fb7ad719eb0730b451ee8b9896f130099255e6
Marc Merle <marcmerle@hotmail.com>
2020-02-18 14:50:34 +0100

    upload photos works without a limit

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -3,7 +3,7 @@
 class Bar < ApplicationRecord
   has_many :bookings
   belongs_to :owner, class_name: "User", foreign_key: "user_id"
-  has_many_attached :photo
+  has_many_attached :photos
 
   validates :name, :address, presence: true, uniqueness: true
   validates :price, :description, :capacity, :opening_start, :opening_end, presence: true
