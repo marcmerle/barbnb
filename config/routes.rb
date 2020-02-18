b851ed3293244d47ed8d2c3716a3bcca5e034c18
Hugo Dornbierer <hdornbierer@gmail.com>
2020-02-18 15:50:22 +0100

    Side Menu done

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
 
-  get '/dashboard/bookings', to: 'bookings#index'
+  get '/dashboard/bookings', to: 'bookings#index', as: "dashboard_bookings"
 
   resources :bars, except: :destroy
   resources :bookings, except: :index
