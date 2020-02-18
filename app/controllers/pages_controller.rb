35a6212918606edf13edac523845fabc26d3768b
Lomig <dunwael@gmail.com>
2020-02-18 16:27:39 +0100

    Avatar created. Integrated to Nav Bar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    redirect_to(bars_path) if current_user
+  end
+
+  def about
   end
 end
