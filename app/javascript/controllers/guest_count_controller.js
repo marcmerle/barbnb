c60ca102367e1edcc45100a4599eaea88ec479b3
Lomig <dunwael@gmail.com>
2020-02-19 03:55:18 +0100

    Guest count implemented. Field is validated client-side.

---
+++ app/javascript/controllers/guest_count_controller.js
@@ -0,0 +1,55 @@
+import { Controller } from "stimulus";
+
+export default class extends Controller {
+  static targets = ["number", "minus", "plus", "countField"];
+
+  add() {
+    const guests = parseInt(this.numberTarget.value);
+    if (guests === parseInt(this.data.get("maxGuest"))) return null;
+
+    this.numberTarget.value = guests + 1;
+    this.update();
+  }
+
+  remove() {
+    const guests = parseInt(this.numberTarget.value);
+    if (guests === 1) return null;
+
+    this.numberTarget.value = guests - 1;
+    this.update();
+  }
+
+  validateField() {
+    if (!/^\d+$/.test(this.numberTarget.value))
+      this.numberTarget.value = this.numberTarget.value.replace(/\D/g, "");
+
+    if (parseInt(this.numberTarget.value) < 1 || this.numberTarget.value === "")
+      this.numberTarget.value = 1;
+
+    if (parseInt(this.numberTarget.value) > parseInt(this.data.get("maxGuest")))
+      this.numberTarget.value = this.data.get("maxGuest");
+
+    this.update();
+  }
+
+  update() {
+    this.enableButton(this.minusTarget);
+    this.enableButton(this.plusTarget);
+
+    this.countFieldTarget.value = this.numberTarget.value;
+
+    if (this.countFieldTarget.value === "1")
+      this.disableButton(this.minusTarget);
+
+    if (this.countFieldTarget.value === this.data.get("maxGuest"))
+      this.disableButton(this.plusTarget);
+  }
+
+  disableButton(button) {
+    button.classList.add("disabled");
+  }
+
+  enableButton(button) {
+    button.classList.remove("disabled");
+  }
+}
