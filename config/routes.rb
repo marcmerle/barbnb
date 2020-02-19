78222f81a8bef6cab4d976fd9dc2d905fbc5f537
Lomig <dunwael@gmail.com>
2020-02-19 11:23:34 +0100

    New Class - Review - Routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
 
   get '/dashboard/bookings', to: 'bookings#index', as: "dashboard_bookings"
 
-  resources :bars, except: :destroy
-  resources :bookings, except: :index
+  resources :bars, except: :destroy do
+    resources :bookings, only: %i[create]
+    resources :reviews, only: %i[create]
+  end
+  resources :bookings, except: %i[index create]
 end
