9c7b39dceb45223af53a4cbadfc6c499440d7cd6
Marc Merle <marcmerle@hotmail.com>
2020-02-20 13:16:33 +0100

    implment search bar

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
 class Bar < ApplicationRecord
+  include PgSearch::Model
   has_many :bookings, dependent: :nullify
   has_many :reviews, through: :bookings
   belongs_to :owner, inverse_of: :bars, class_name: "User", foreign_key: "user_id"
   has_many_attached :photos
   geocoded_by :address
+
+  pg_search_scope :bar_search,
+    against: [:name, :address],
+    using: {
+      tsearch: { prefix: true }
+    }
+
   after_validation :geocode, if: :will_save_change_to_address?
   validates :name, :address, presence: true # , uniqueness: true
   validates :price, :description, :capacity, :opening_start, :opening_end, presence: true
